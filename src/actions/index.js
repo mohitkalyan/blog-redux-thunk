a1b2779b051db813d86f6975d0494eef565fb7e2
Mohit Kalyan <mohit.kalyan@gmail.com>
2020-11-23 21:37:02 +0530

    lodash included

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,22 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
+import _ from "lodash";
 export const fetchPost = () => {
-  return {
-    type: "FETCH_POST",
+  return async (dispatch) => {
+    const response = await jsonPlaceholder.get("/posts");
+    dispatch({ type: "FETCH_POST", payload: response.data });
   };
 };
+
+export const fetchUser = (id) => async (dispatch) => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+  dispatch({ type: "FETCH_USER", payload: response.data });
+};
+
+// export const fetchUser = (id) => (dispatch) => {
+//   _fetchUser(id, dispatch);
+// };
+
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+//   dispatch({ type: "FETCH_USER", payload: response.data });
+// });
